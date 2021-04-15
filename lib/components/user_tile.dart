d0656d0cb48dba9e7249337787cbbbd8a1dc449f
hiago <hiagohps360@gmail.com>
2021-04-15 11:03:00 -0300

    feat: delete user list

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,6 +1,8 @@
 import 'package:crud_flutter/models/user.dart';
+import 'package:crud_flutter/provider/users.dart';
 import 'package:crud_flutter/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
@@ -33,7 +35,30 @@ class UserTile extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.delete),
               color: Colors.red,
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Excluir Usuário'),
+                    content: Text('Tem certeza???'),
+                    actions: <Widget>[
+                      TextButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          child: Text('Não')),
+                      TextButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          child: Text('Sim')),
+                    ],
+                  ),
+                ).then(
+                  (confirmed) => {
+                    if (confirmed)
+                      {
+                        Provider.of<Users>(context, listen: false).remove(user),
+                      }
+                  },
+                );
+              },
             ),
           ],
         ),
