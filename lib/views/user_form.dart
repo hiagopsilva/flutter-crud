55e83d81906ee0691b99438d0c41dab9d1e1c905
hiago <hiagohps360@gmail.com>
2021-04-15 10:51:29 -0300

    chore: list data user

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -7,8 +7,21 @@ class UserForm extends StatelessWidget {
   final _form = GlobalKey<FormState>();
   final Map<String, String> _formData = {};
 
+  void _loadFormData(User user) {
+    if (user != null) {
+      _formData['id'] = user.id;
+      _formData['name'] = user.name;
+      _formData['email'] = user.email;
+      _formData['avatarUrl'] = user.avatarUrl;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final User user = ModalRoute.of(context).settings.arguments;
+
+    _loadFormData(user);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de Usuário'),
@@ -42,6 +55,7 @@ class UserForm extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 TextFormField(
+                  initialValue: _formData['name'],
                   decoration: InputDecoration(labelText: 'Nome'),
                   validator: (value) {
                     if (value == null || value.trim().isEmpty) {
@@ -53,6 +67,7 @@ class UserForm extends StatelessWidget {
                   onSaved: (value) => _formData['name'] = value,
                 ),
                 TextFormField(
+                  initialValue: _formData['email'],
                   decoration: InputDecoration(labelText: 'Email'),
                   validator: (value) {
                     if (value.trim().length < 3) {
@@ -64,6 +79,7 @@ class UserForm extends StatelessWidget {
                   onSaved: (value) => _formData['email'] = value,
                 ),
                 TextFormField(
+                  initialValue: _formData['avatarUrl'],
                   decoration: InputDecoration(labelText: 'URL do avatar'),
                   onSaved: (value) => _formData['avatarUrl'] = value,
                 )
