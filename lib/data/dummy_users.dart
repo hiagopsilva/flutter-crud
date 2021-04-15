7e8b37f680e59db62f9decdf822b0992c2e97354
hiago <hiagohps360@gmail.com>
2021-04-14 23:49:48 -0300

    feat: data dummy users

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:crud_flutter/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'),
+  '4': const User(
+      id: '4',
+      name: 'Guilherme',
+      email: 'guilherme@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'),
+};
