cb3ebbfd26d32702ad68b366fa87a2e157d9f1cd
hiago <hiagohps360@gmail.com>
2021-04-14 23:49:08 -0300

    feat: component user tile

---
+++ lib/components/user_tile.dart
@@ -0,0 +1,18 @@
+import 'package:crud_flutter/models/user.dart';
+import 'package:flutter/material.dart';
+
+class UserTile extends StatelessWidget {
+  final User user;
+
+  const UserTile(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    final avatar = user.avatarUrl == null || user.avatarUrl.isEmpty
+        ? CircleAvatar(child: Icon(Icons.person))
+        : CircleAvatar(backgroundImage: NetworkImage(user.avatarUrl));
+    return ListTile(
+      leading: avatar,
+    );
+  }
+}
