14f5910e11565e9e2de8bf155da50e7134ceab16
hiago <hiagohps360@gmail.com>
2021-04-15 12:36:15 -0300

    feat: loading

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -10,6 +10,7 @@ class UserForm extends StatefulWidget {
 
 class _UserFormState extends State<UserForm> {
   final _form = GlobalKey<FormState>();
+  bool _isLoading = false;
 
   final Map<String, String> _formData = {};
 
@@ -44,6 +45,10 @@ class _UserFormState extends State<UserForm> {
               if (isValid) {
                 _form.currentState.save();
 
+                setState(() {
+                  _isLoading = true;
+                });
+
                 await Provider.of<Users>(context, listen: false).put(
                   User(
                     id: _formData['id'],
@@ -53,66 +58,72 @@ class _UserFormState extends State<UserForm> {
                   ),
                 );
 
+                setState(() {
+                  _isLoading = false;
+                });
+
                 Navigator.of(context).pop();
               }
             },
           )
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(15),
-        child: Form(
-            key: _form,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  initialValue: _formData['name'],
-                  decoration: InputDecoration(labelText: 'Nome'),
-                  validator: (value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return 'Nome inválido';
-                    }
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: EdgeInsets.all(15),
+              child: Form(
+                  key: _form,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        initialValue: _formData['name'],
+                        decoration: InputDecoration(labelText: 'Nome'),
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return 'Nome inválido';
+                          }
 
-                    return null;
-                  },
-                  onSaved: (value) => _formData['name'] = value,
-                ),
-                TextFormField(
-                  initialValue: _formData['email'],
-                  decoration: InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value.trim().length < 3) {
-                      return 'Email inválido';
-                    }
+                          return null;
+                        },
+                        onSaved: (value) => _formData['name'] = value,
+                      ),
+                      TextFormField(
+                        initialValue: _formData['email'],
+                        decoration: InputDecoration(labelText: 'Email'),
+                        validator: (value) {
+                          if (value.trim().length < 3) {
+                            return 'Email inválido';
+                          }
 
-                    return null;
-                  },
-                  onSaved: (value) => _formData['email'] = value,
-                ),
-                TextFormField(
-                  initialValue: _formData['avatarUrl'],
-                  decoration: InputDecoration(labelText: 'URL do avatar'),
-                  onSaved: (value) => {
-                    if (value == 'pinguim')
-                      {
-                        value =
-                            'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
-                      },
-                    _formData['avatarUrl'] = value,
-                  },
-                ),
-                Container(
-                  margin:
-                      const EdgeInsets.only(left: 0, right: 65.0, top: 10.0),
-                  child: Text(
-                    'Está sem avatar? Escreva "pinguim" na URL do avatar.',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(color: Colors.blueGrey[400]),
-                  ),
-                )
-              ],
-            )),
-      ),
+                          return null;
+                        },
+                        onSaved: (value) => _formData['email'] = value,
+                      ),
+                      TextFormField(
+                        initialValue: _formData['avatarUrl'],
+                        decoration: InputDecoration(labelText: 'URL do avatar'),
+                        onSaved: (value) => {
+                          if (value == 'pinguim')
+                            {
+                              value =
+                                  'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+                            },
+                          _formData['avatarUrl'] = value,
+                        },
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(
+                            left: 0, right: 65.0, top: 10.0),
+                        child: Text(
+                          'Está sem avatar? Escreva "pinguim" na URL do avatar.',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.blueGrey[400]),
+                        ),
+                      )
+                    ],
+                  )),
+            ),
     );
   }
 }
